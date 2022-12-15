90899c29c985a6addb9a8b551a69ca4984b38816
coldfuzz <kurtulusyilmaz1@gmail.com>
2022-12-15 18:48:00 +0000

    Update image_version_checker.py

--- image_version_checker.py
+++ image_version_checker.py
@@ -3,7 +3,7 @@ import sys
 
 version_command = f"head -n 1 launchpad/{sys.argv[1]}/version.txt"
 version = subprocess.getoutput(version_command)
-image_command = f"gcloud artifacts docker images list europe-west2-docker.pkg.dev/kaluza-analytics-prod/launchpad/{sys.argv[1]} --include-tags --format=json | jq --raw-output '.[].tags' | grep -w  {version}"
+image_command = f"gcloud artifacts docker images list us-central1-docker.pkg.dev/cosmic-bonus-358914/kurt/{sys.argv[1]} --include-tags --format=json | jq --raw-output '.[].tags' | grep -w  {version}"
 result = subprocess.getoutput(image_command)
 
 if result.split("\n")[2] == version.strip(" \n"):
