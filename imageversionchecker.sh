c9a155b7c69778134e31867219c301d8a67378f7
coldfuzz <kurtulusyilmaz1@gmail.com>
2022-12-15 18:48:53 +0000

    Update imageversionchecker.sh

--- imageversionchecker.sh
+++ imageversionchecker.sh
@@ -1,4 +1,4 @@
 deployment=$1
 version=$(head -n 1 launchpad/$deployment/version.txt)
-version_text=$(gcloud artifacts docker images list europe-west2-docker.pkg.dev/kaluza-analytics-prod/launchpad/$deployment --include-tags --format=json | jq --raw-output '.[].tags' | grep -w  $version)
+version_text=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/cosmic-bonus-358914/kurt/$deployment --include-tags --format=json | jq --raw-output '.[].tags' | grep -w  $version)
 [[ $version_text = $version ]] && echo "equal" && exit 125
