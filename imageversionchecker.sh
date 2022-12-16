15f0be8528beea7b3be505937633c80793ebc179
coldfuzz <kurtulusyilmaz1@gmail.com>
2022-12-16 15:31:06 +0000

    Update imageversionchecker.sh

--- imageversionchecker.sh
+++ imageversionchecker.sh
@@ -2,7 +2,7 @@ deployment=$1
 version=$(head -n 1 launchpad/$deployment/version.txt)
 version_text=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/cosmic-bonus-358914/den/$deployment --include-tags --format=json | grep -o '"tags": "[^"]*'  | grep -o '[^"]*$')
 if [ "$version_text" == "$version" ]; then
-    echo "Both Strings are Equal." && exit 125
+    echo "Version already is pushed" && exit 125
 else
-    echo "Both Strings are not Equal."
+    echo "New Version of Image"
 fi
