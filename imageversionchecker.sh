457b473bdfccd0ef912761e422d9c85b065133f3
coldfuzz <kurtulusyilmaz1@gmail.com>
2022-12-16 14:22:38 +0000

    Update imageversionchecker.sh

--- imageversionchecker.sh
+++ imageversionchecker.sh
@@ -1,4 +1,4 @@
 deployment=$1
 version=$(head -n 1 launchpad/$deployment/version.txt)
-version_text=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/cosmic-bonus-358914/quickstart-docker-repo/$deployment --include-tags --format=json | jq --raw-output '.[].tags' | grep -w  $version)
+version_text=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/cosmic-bonus-358914/den/$deployment --include-tags --format=json | jq --raw-output '.[].tags' | grep -w  $version)
 [[ $version_text = $version ]] && echo "equal" && exit 125
